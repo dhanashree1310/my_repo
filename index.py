cc7d944d861a90901813212a485efc163d42e1ea
dhanashree1310 <dhanashreejadhav250@gmail.com>
2026-03-17 17:05:41 +0530

    this is v2

--- index.py
+++ index.py
@@ -2,5 +2,9 @@ print("Hello, World!")
 print("Welcome to Python programming.")
 print("This is a simple script to demonstrate basic Python syntax.")
 print("You can run this code to see the output.")
+print(5+7)
+print("The result of 5 + 7 is:", 5+7)
+
+
 
 
