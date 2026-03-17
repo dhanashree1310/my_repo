a6a7acd8ed767b86c59262ce37bfc60531698494
dhanashree1310 <dhanashreejadhav250@gmail.com>
2026-03-17 17:51:17 +0530

    version 4

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ print("Welcome to Python programming.")
 print("This is a simple script to demonstrate basic Python syntax.")
 print("You can run this code to see the output.")
 print(5+7)
-print("The result of 5 + 7 is:", 5+7)
+print("The result of 5 + 7 is:", 5+8)
 print("Python is a versatile programming language.")
 
 
