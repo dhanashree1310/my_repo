d3bcd79d8069b328d245b8a0c6955659bcf385bd
dhanashree1310 <dhanashreejadhav250@gmail.com>
2026-03-17 16:52:26 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,6 @@
+print("Hello, World!")
+print("Welcome to Python programming.")
+print("This is a simple script to demonstrate basic Python syntax.")
+print("You can run this code to see the output.")
+
+
